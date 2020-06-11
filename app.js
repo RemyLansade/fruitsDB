fde6f9b4c93be9605457e558e96675f9890599e5
bobbystu <boblol77@live.fr>
2020-06-11 18:58:46 +0200

    Update fruitSchema

--- app.js
+++ app.js
@@ -4,32 +4,78 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/fruitsDB', { useNewUrlParser: true, useUnifiedTopology: true});
 
 const fruitSchema = new mongoose.Schema({
-    name: String,
-    rating: Number,
+    name: {
+        type: String,
+        required: [true, "Please check your data entry, no name specified."]
+    },
+    rating: {
+        type: Number,
+        min: 1,
+        max: 10
+    },
     review: String
 });
 
 const Fruit = mongoose.model("Fruit", fruitSchema);
 
-const fruit = new Fruit({
-    name: "Apple",
-    rating: 7,
-    review: "Pretty solid as a fruit"
-});
+
+
+// const apple = new Fruit({
+//     name: "Apple",
+//     rating: 7,
+//     review: "Pretty solid as a fruit"
+// });
+
+// const orange = new Fruit({
+//     name: "Orange",
+//     rating: 7,
+//     review: "Pretty solid as a fruit"
+// });
+
+// const banana = new Fruit({
+//     name: "Banana",
+//     rating: 7,
+//     review: "Pretty solid as a fruit"
+// });
+
+
+
+// Fruit.insertMany([apple,orange,banana], (err) => {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log("Succesfully saved all the fruits to fruitsDB");
+//     }
+// });
+
+// Fruit.find((err, fruits) => {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         fruits.forEach(fruit => {
+//             mongoose.connection.close();
+//             console.log(fruit.name);
+//         });
+//     }
+// });
 
 // fruit.save();
 
-const personSchema = new mongoose.Schema({
-    name: String,
-    age: Number
-});
 
-const Person = mongoose.model("Person", personSchema);
 
-const person = new Person({
-    name: "John",
-    age: 37
-})
+
+
+// const personSchema = new mongoose.Schema({
+//     name: String,
+//     age: Number
+// });
+
+// const Person = mongoose.model("Person", personSchema);
+
+// const person = new Person({
+//     name: "John",
+//     age: 37
+// })
 
 // person.save();
 
